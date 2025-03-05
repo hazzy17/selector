625ec2d4174aac7815f0312ff6fbbc2f985dc7bf
hazzy17 <peteronchoke62@gmail.com>
2025-03-05 10:17:09 +0300

    this is my first class, and I am learning how to use git

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
-print("hello")
+class Race:
+    def __init__(self, brand, speed, position):
+        self.brand = brand
+        self.speed = speed
+        self.position = position
+
+    def position(self):
+        return f"{self.brand} is at position {self.position} with speed {self.speed}kph."
+    
+    def switch_position(self, new_position):
+        self.position = self.new_position 
+        return f"{self.brand} is now at position {self.position} with speed {self.speed}kph."
+    
+    def speed_up(self, new_speed):
+        self.speed = self.new_speed
+        return f"{self.brand} is now at position {self.position} with speed {self.speed}kph."
+    
+    def slow_down(self, new_speed):
+        self.speed = self.new_speed
+        return f"{self.brand} is now at position {self.position} with speed {self.speed}kph."
+    
+
+toyota = Race("Toyota", "100", "1")
+print(toyota.position())
+
+

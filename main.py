23ebd616a248c2c10ebeddfebc2c408583d5da1d
hazzy17 <peteronchoke62@gmail.com>
2025-03-07 17:32:35 +0300

    you can only return integers where necessary for future updates

--- main.py
+++ main.py
@@ -4,23 +4,23 @@ class Race:
         self.speed = speed
         self.position = position
 
-    def position(self):
+    def get_position(self):
         return f"{self.brand} is at position {self.position} with speed {self.speed}kph."
     
     def switch_position(self, new_position):
-        self.position = self.new_position 
+        self.position = new_position 
         return f"{self.brand} is now at position {self.position} with speed {self.speed}kph."
     
     def speed_up(self, new_speed):
-        self.speed = self.new_speed
+        self.speed = new_speed
         return f"{self.brand} is now at position {self.position} with speed {self.speed}kph."
     
     def slow_down(self, new_speed):
-        self.speed = self.new_speed
+        self.speed = new_speed
         return f"{self.brand} is now at position {self.position} with speed {self.speed}kph."
     
 
-toyota = Race("Toyota", "100", "1")
-print(toyota.position())
+toyota = Race("Toyota", 100, 1)
+print(toyota.get_position())
 
 

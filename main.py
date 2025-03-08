e5533196adc205fcf4cee9c4a19ed1b8f9c10560
hazzy17 <peteronchoke62@gmail.com>
2025-03-09 01:21:58 +0300

    i just created an instance of the class so that i can call multiple objects from a list, tommorow im going to manipulate the list to make it more dynamic and add more objects to the list

--- main.py
+++ main.py
@@ -5,22 +5,34 @@ class Race:
         self.position = position
 
     def get_position(self):
-        return f"{self.brand} is at position {self.position} with speed {self.speed}kph."
+        return f"{self.brand} is at position {self.position}."
     
     def switch_position(self, new_position):
         self.position = new_position 
-        return f"{self.brand} is now at position {self.position} with speed {self.speed}kph."
+        return f"{self.brand} is now at position {self.position}."
+    
+
+    def initial_speed(self):
+        return f"{self.brand} is at {self.speed}kph."
     
     def speed_up(self, new_speed):
         self.speed = new_speed
-        return f"{self.brand} is now at position {self.position} with speed {self.speed}kph."
+        return f" {self.brand} has sped up to {self.speed}kph." 
     
     def slow_down(self, new_speed):
         self.speed = new_speed
-        return f"{self.brand} is now at position {self.position} with speed {self.speed}kph."
+        return f"{self.brand} has slown down to {self.speed}kph."
     
+cars = [
+    Race('Mazda', 90, 2),
+    Race('Toyota', 100, 1),
+    Race('Honda', 80, 3),
+    Race('Ford', 70, 4)
+]
+cars.sort(key=lambda x: x.speed)
+for racers in cars:
+    print(racers.get_position())
 
-toyota = Race("Toyota", 100, 1)
-print(toyota.get_position())
+ 
 
 
